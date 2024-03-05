4d2390159771d1d614a70c0d511730833c52c2b8
kinna <kinnahuang717@gmail.com>
2024-03-05 23:50:48 +0800

    feat: 新增fontawesome

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@ import './style.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(ElementPlus)
-app.mount('#app')
+// fontawesome
+library.add(fas);
+app.component('font-awesome-icon', FontAwesomeIcon);
+
+app.use(ElementPlus);
+
+app.mount('#app');
